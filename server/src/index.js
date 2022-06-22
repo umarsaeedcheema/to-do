eed2d9e895f5924f23ea8c9753117e0cdb2d20cb
umar <23100206@lums.edu.pk>
2022-06-22 13:00:14 +0500

    git

--- server/src/index.js
+++ server/src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/todos", (req, res) => {
-  res.send("Hello World");
+  res.send("Todos end point hit");
 });
 
 app.listen(8080);
